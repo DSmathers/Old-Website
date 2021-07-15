4ccbec3b2c6c5bf2537045a14738c9a02c9392d0
DSmathers <87347686+DSmathers@users.noreply.github.com>
2021-07-14 22:46:06 -0700

    updates to css

--- behavior.js
+++ behavior.js
@@ -1,6 +1,9 @@
+/*  function adds "responsive" to class name of main_nav for hamburger menu
 function reactiveNav () {
     console.log('testing')
     var x = document.getElementById('main_nav');
     if(x.className === 'main_nav')  {
         x.className += 'responsive';
 } else  {x.className = 'main_nav';}}
+*/
+
