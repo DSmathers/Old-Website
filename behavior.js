afef573e1ac76867468b3fa03a9d48ea655d69dd
DSmathers <87347686+DSmathers@users.noreply.github.com>
2021-07-14 18:30:54 -0700

    Update7.14.630
    played with media queries, practicing building a responsive page for all devices

--- behavior.js
+++ behavior.js
@@ -1,3 +1,6 @@
 function reactiveNav () {
     console.log('testing')
-}
+    var x = document.getElementById('main_nav');
+    if(x.className === 'main_nav')  {
+        x.className += 'responsive';
+} else  {x.className = 'main_nav';}}
