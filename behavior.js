4b0bae427794d63940dbf48d2892938348c0d293
DSmathers <87347686+DSmathers@users.noreply.github.com>
2021-07-15 19:34:15 -0700

    updates7.15
    Removed hamburger menu and redid nav bar to be responsive. instead of displaying logo in navbar (which was just my name) i will display it in the banner section.

--- behavior.js
+++ behavior.js
@@ -1,9 +1,4 @@
-/*  function adds "responsive" to class name of main_nav for hamburger menu
-function reactiveNav () {
-    console.log('testing')
-    var x = document.getElementById('main_nav');
-    if(x.className === 'main_nav')  {
-        x.className += 'responsive';
-} else  {x.className = 'main_nav';}}
-*/
+
+
+
 
